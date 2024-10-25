a351bc632cc00f72159fec0e6abb8c777e440299
ejais58 <ejais58@gmail.com>
2024-10-25 18:13:33 -0300

    add comment filters search

--- src/services/playerService.js
+++ src/services/playerService.js
@@ -5,13 +5,17 @@ const { Op } = require('sequelize');
 const getAllPlayers = async (page, limit, filters) => {
 
     const where = {};
+    //[Op.like] realizar búsquedas "como". Es útil para búsquedas parciales, por ejemplo: 'Messi' coincidirá con cualquier texto que contenga 'Messi'.
+    // Si `filters.name` tiene valor, agrega una condición para `long_name`
     if (filters.name) {
         where.long_name = { [Op.like]: `%${filters.name}%` };
     }
 
+    // Si `filters.club` tiene valor, agrega una condición para `club_name`
     if (filters.club) {
         where.club_name = { [Op.like]: `%${filters.club}%` };
     }
+    // Si `filters.position` tiene valor, agrega una condición para `player_positions`
     if (filters.position) {
         where.player_positions = { [Op.like]: `%${filters.position}%` };
     }
